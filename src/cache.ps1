464b6a6aa74a65f0b30ae093a6bcc4f97478b497
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2024-08-19 23:23:52 -0500

    Fix double compression

--- src/cache.ps1
+++ src/cache.ps1
@@ -4,6 +4,3 @@ $resources = Find-PSResource -Name * -Repository PSGallery
 
 $resources | Export-Clixml psgallery.xml
 $resources | ConvertTo-Json -Compress | Out-File psgallery.json
-
-Compress-Archive psgallery.xml psgallery-xml.zip
-Compress-Archive psgallery.json psgallery-json.zip
